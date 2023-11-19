8951164bedd7090788f99b474d7c53bc16bdc5ce
gulraiz <gulraiznoorbari@gmail.com>
2023-11-19 05:10:13 +0500

    Add rideSlice to rootReducer

--- store/reducers.js
+++ store/reducers.js
@@ -3,13 +3,14 @@ import { persistCombineReducers } from "redux-persist";
 
 import userReducer from "./slices/userSlice";
 import driverReducer from "./slices/driverSlice";
+import rideReducer from "./slices/rideSlice";
 import locationReducer from "./slices/locationSlice";
 import navigationReducer from "./slices/navigationSlice";
 
 const persistConfig = {
     key: "rootReducer",
     storage: AsyncStorage,
-    whitelist: ["user", "driver", "location", "navigation"],
+    whitelist: ["user", "driver", "ride", "location", "navigation"],
     blacklist: [],
     timeout: 7000,
 };
@@ -17,6 +18,7 @@ const persistConfig = {
 const rootReducer = persistCombineReducers(persistConfig, {
     user: userReducer,
     driver: driverReducer,
+    ride: rideReducer,
     location: locationReducer,
     navigation: navigationReducer,
 });
