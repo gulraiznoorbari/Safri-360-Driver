51bddc9332dcfd052d9d7ae69fc6aba6c6671801
gulraiz <gulraiznoorbari@gmail.com>
2023-11-26 05:08:54 +0500

    Add lottie-ios dependency and MapIcon

--- assets/index.js
+++ assets/index.js
@@ -2,3 +2,4 @@ export { default as UserIcon } from "../assets/misc/user.png";
 export { default as LicensePlateIcon } from "../assets/misc/number_plate.png";
 export { default as CarIcon } from "../assets/icons/car.png";
 export { default as DriverIcon } from "../assets/icons/driver.png";
+export { default as MapIcon } from "../assets/icons/map.png";
